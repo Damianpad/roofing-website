e23803d1351a7db2e1f7e34a387da0a6a292e46a
Damian Padilla <damipad4@gmail.com>
2023-02-06 13:36:30 -0600

    updated mobile view

--- main/main.js
+++ main/main.js
@@ -1,17 +1,22 @@
 // let nav = document.querySelector('#main-nav').addEventListener('scroll', changeNavColor)
 // document.querySelector('#nav').addEventListener('scroll', changeNavColor)
 let nav = document.querySelector('nav')
+let bars = document.querySelector('.fa-bars')
 
 window.onscroll = function changeNavColor() {
     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
         console.log(nav.scrollTop)
         nav.classList.remove('nav-dark')
         nav.classList.add('nav-white')
+        bars.classList.add('fa-bars-scroll')       
+
     } else {
         console.log(nav.scrollTop)
         console.log('light nav');
         nav.classList.add('nav-dark')
-        nav.classList.remove('nav-color')
+        nav.classList.remove('nav-white')
+        bars.classList.add('fa-bars')
+        bars.classList.remove('fa-bars-scroll')
     }
 };
 
